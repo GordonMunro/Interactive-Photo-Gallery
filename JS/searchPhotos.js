ded4396554bd4bd132459f1d08aad62d2a8064e4
GordonMunro <82704461+GordonMunro@users.noreply.github.com>
2021-08-21 15:13:46 -0700

    converted to arrow function

--- JS/searchPhotos.js
+++ JS/searchPhotos.js
@@ -1,8 +1,8 @@
 // NB I got a lot of help on this from the slack Office Hours and
 // https://www.w3schools.com/howto/howto_js_filter_lists.asp
-//https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/
+// https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/
 
-function searchCap() {
+const searchCap = () => {
     let inputSearch = document.getElementById('search');
     let search = inputSearch.value.toLowerCase();
     let tag = document.getElementsByTagName('a');
